d2bfb9b64f812d7216fa1395f32264c697843463
MUTED64 <muted32555664@gmail.com>
2023-08-07 00:54:39 +0800

    Change Backend

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,14 +4,14 @@
  * @type {string}
  */
 
-let baseUrl = 'https://49.232.157.251';
+let baseUrl = 'https://49.232.157.251:8888';
 
 if (process.env.NODE_ENV === "development") {
-    baseUrl = "https://49.232.157.251";
+    baseUrl = "https://49.232.157.251:8888";
 } else if (process.env.NODE_ENV === "test") {
-    baseUrl = "https://49.232.157.251";
+    baseUrl = "https://49.232.157.251:8888";
 } else if (process.env.NODE_ENV === "production") {
-    baseUrl = "https://49.232.157.251";
+    baseUrl = "https://49.232.157.251:8888";
 }
 
 let isProduction = false;
